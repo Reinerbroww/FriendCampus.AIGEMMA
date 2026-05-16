cb6b211467bae6680d4b01e17817e9a0161db6ad
Reinerbroww <reinersakunab01@gmail.com>
2026-05-16 13:09:58 +0800

    fix gemma

--- gemma.py
+++ gemma.py
@@ -8,7 +8,7 @@ from google.genai import types
 load_dotenv()
 
 api_key = os.environ.get("GEMINI_API_KEY")
-print("ENV CHECK:", os.environ.get("GEMINI_API_KEY"))
+
 if not api_key:
     raise ValueError("GEMINI_API_KEY not found in environment variables.")
 
